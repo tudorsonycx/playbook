6b956ffb2f91aee1c8fac02053a4c2d52e017c86
tudorsonycx <tudorsonycx@gmail.com>
2025-03-17 11:51:17 +0000

    inject DataRepository to application controller and its tests

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import models.DataModel
+import play.api.libs.json.Json
+
 import javax.inject._
 import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import repositories.DataRepository
@@ -10,8 +13,15 @@ import scala.concurrent.ExecutionContext
 class ApplicationController @Inject()(
   val controllerComponents: ControllerComponents,
   val dataRepository: DataRepository,
-  val executionContext: ExecutionContext) extends BaseController {
-  def index(): Action[AnyContent] = Action(Ok)
+  implicit val executionContext: ExecutionContext) extends BaseController {
+  def index(): Action[AnyContent] = Action.async { implicit request =>
+    dataRepository.index().map {
+      case Right(item: Seq[DataModel]) => Ok {
+        Json.toJson(item)
+      }
+      case Left(error) => Status(error)(Json.toJson("Unable to find any books"))
+    }
+  }
 
   def create() = TODO
 
