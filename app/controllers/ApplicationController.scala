379ffe0da38fd3e25b240e911a46a1d7329ceda0
tudorsonycx <tudorsonycx@gmail.com>
2025-03-17 11:16:41 +0000

    add CRUD methods to ApplicationController and add routes

--- app/controllers/ApplicationController.scala
+++ app/controllers/ApplicationController.scala
@@ -4,4 +4,14 @@ import javax.inject._
 import play.api.mvc.{BaseController, ControllerComponents}
 
 @Singleton
-class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+  def index() = TODO
+
+  def create() = TODO
+
+  def read(id: String) = TODO
+
+  def update(id: String) = TODO
+
+  def delete(id: String) = TODO
+}
